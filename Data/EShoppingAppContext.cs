cd903da96584b1e98e30e9ed9d8b79bbacc2f638
Devang Shakya <devang.shakya@intech-systems.com>
2022-06-29 15:51:35 +0530

    Add project files.

---
+++ Data/EShoppingAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EShoppingApp.Models;
+
+namespace EShoppingApp.Data
+{
+    public class EShoppingAppContext : DbContext
+    {
+        public EShoppingAppContext (DbContextOptions<EShoppingAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EShoppingApp.Models.Userdetails> Userdetails { get; set; }
+    }
+}
---
+++ Models/Userdetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShoppingApp.Models
+{
+    public class Userdetails
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Mobileno { get; set; }
+        public int Created { get; set; }
+        public string Email { get; set; }
+        public int Usertype { get; set; }
+
+        public string Password { get; set; }
+    }
+}
